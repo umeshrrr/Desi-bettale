a0372e8a48c1d7318deb2d523c2963bc52132c02
umeshrrr <33366879+umeshrrr@users.noreply.github.com>
2026-02-21 13:54:21 +0530

    Create ObjectPooler.cs

---
+++ ObjectPooler.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+using DesiShadow.Core; // Access Singleton
+
+namespace DesiShadow.Gameplay
+{
+    public class ObjectPooler : Singleton<ObjectPooler>
+    {
+        [System.Serializable]
+        public class Pool
+        {
+            public string tag;
+            public GameObject prefab;
+            public int size;
+        }
+
+        public List<Pool> pools;
+        public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+        void Start()
+        {
+            poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+            foreach (Pool pool in pools)
+            {
+                Queue<GameObject> objectPool = new Queue<GameObject>();
+
+                for (int i = 0; i < pool.size; i++)
+                {
+                    GameObject obj = Instantiate(pool.prefab);
+                    obj.SetActive(false);
+                    objectPool.Enqueue(obj);
+                }
+
+                poolDictionary.Add(pool.tag, objectPool);
+            }
+        }
+
+        public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+        {
+            if (!poolDictionary.ContainsKey(tag))
+            {
+                Debug.LogWarning($"Pool with tag {tag} doesn't exist.");
+                return null;
+            }
+
+            GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+            objectToSpawn.SetActive(true);
+            objectToSpawn.transform.position = position;
+            objectToSpawn.transform.rotation = rotation;
+
+            // Interface check: If object has a reset function (like health), call it here
+            
+            poolDictionary[tag].Enqueue(objectToSpawn); // Put back in line
+            return objectToSpawn;
+        }
+    }
+}
